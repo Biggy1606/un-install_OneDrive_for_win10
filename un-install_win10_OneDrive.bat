c4214d58a61e5e64f033c1c8199b85156770be23
Igor Barcik <igor1606553@gmail.com>
2019-10-30 01:13:06 +0100

    "please wait" added

--- un-install_win10_OneDrive.bat
+++ un-install_win10_OneDrive.bat
@@ -5,12 +5,14 @@ set /p selection=
 echo.
 if %selection% == 1 (
 echo Install OneDrive
+echo Please wait...
 %systemroot%\SysWOW64\OneDriveSetup.exe
-echo Done..
+echo Done.
 @pause
 )
 if %selection% == 2 (
 echo UnInstall OneDrive
+echo Please wait...
 %systemroot%\SysWOW64\OneDriveSetup.exe /uninstall
 echo Done..
 @pause
